46f366d4ea7fbc172e2221e13571b47af562a96c
Andrea Kennedy <asrkennedy@gmail.com>
2013-07-14 19:55:53 +0100

    add Game Scores div on index view for Game/home page with ruby for game name, top user, and top score, with some css

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,8 +1,8 @@
 class Match < ActiveRecord::Base
   has_many :moves
   belongs_to :game
-  belongs_to :player1, classname: "User"
-  belongs_to :player2, classname: "User"
+  belongs_to :player1, class_name: "User"
+  belongs_to :player2, class_name: "User"
 
   attr_accessible :game_id, :loser, :player1_id, :player2_id, :winner
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Match.create! game_id: 1, player1_id: 3, player2_id: 4, winner: 3, loser: 4
+
+User.create! name:'Stu', gender: 'male', user_image: '#', username: "sknight126", password: "stu", bio: "Stu Knight is awesome"
+
+User.create! name:'Andrea', gender: 'female', user_image: '#', username: "andreayea", password: "password", bio: "Andrea is the coolest"
