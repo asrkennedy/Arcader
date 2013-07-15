e344ef3c5df1da7bde2e027cec90ef8d2051156a
Andrea Kennedy <asrkennedy@gmail.com>
2013-07-15 11:53:33 +0100

    add current users div with links and fixed ruby for score table using helper

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -1,2 +1,17 @@
 module GameHelper
+  def top_scorer_for_game(game)
+    winners = Match.where(game_id: game.id).group_by(&:winner)
+    if winners.size < 1
+      return "No winners"
+    end
+    biggest = winners.first
+    winners.each do |winner, wins|
+      if wins.size > biggest.size
+        biggest = winner
+      end
+    end
+    [biggest.first, biggest[1].size]
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   has_many :matches
 
   attr_accessible :bio, :gender, :name, :password, :password_confirmation, :user_image, :username
+
+  mount_uploader :user_image, UserImageUploader
 end
