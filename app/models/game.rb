b4013bda881407ad426aab152aea6e23ea9facd7
Andrea Kennedy <asrkennedy@gmail.com>
2013-07-15 13:30:11 +0100

    James made some cool changes

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,5 @@
 class Game < ActiveRecord::Base
-has_many :matches
-
+  has_many :matches
+  default_scope order('name ASC')
   attr_accessible :description, :game_image, :instructions, :name
-
-  mount_uploader :game_image, GameImageUploader
 end
